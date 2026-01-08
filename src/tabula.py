456122960684bf074f15b06c7895f063b283348c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-08 20:22:32 +0200

    Add initial implementation of tabula.py with basic print statements

---
+++ src/tabula.py
@@ -0,0 +1,13 @@
+print("Sveiki! Pasaule :) ")
+print("Tā ir mana pirmā programma.")
+
+print("Sveiki!\nLabrīt!\nUz redzešanos!")
+
+print("Priekšmets\tAtzīme")
+print("Matemātika\t9")
+print("Fizika\t\t8")
+print("Programmēšana\t10")
+
+print("=" * 30)
+print("SADAĻA: DATI")
+print("=" * 30)
